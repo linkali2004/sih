55a9d84e154c416aa29b1fe20a903efc8943e6e3
linkali2004 <22je0919@iitism.ac.in>
2024-08-25 08:05:10 +0530

    againFinally

--- components/BackgroundBeamsWithCollisionDemo.tsx
+++ components/BackgroundBeamsWithCollisionDemo.tsx
@@ -7,7 +7,7 @@ export function BackgroundBeamsWithCollisionDemo() {
     <BackgroundBeamsWithCollision>
       <div className="flex flex-col items-center justify-center h-full px-4 md:px-8 lg:px-12">
         <h2 className="text-2xl relative z-20 md:text-4xl lg:text-5xl xl:text-7xl font-bold text-center text-black dark:text-white font-sans tracking-tight">
-          Sushrut Aushadruchi
+          Sushrut Aushadsuchi
         </h2>
         <div className="relative mx-auto inline-block w-full max-w-3xl mt-2 sm:mt-6 md:mt-8 lg:mt-4 [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
           <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
